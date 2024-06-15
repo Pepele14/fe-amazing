1cff853ae03ce2980713ab8ee774319a6a64774d
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-06-15 18:03:46 +0200

    Add routes and like model

--- src/components/PublicArea.jsx
+++ src/components/PublicArea.jsx
@@ -1,40 +1,125 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Public-area.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const PublicArea = () => {
   const [publicNotes, setPublicNotes] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true); // state to potentially load more contents if any available
+  const [userLikes, setUserLikes] = useState([]);
+  const fetchedPages = useRef(new Set()); // Track fetched pages
 
   useEffect(() => {
-    const fetchPublicNotes = async () => {
-      try {
-        const response = await fetch(`${API_URL}/api/notes/public`);
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-        const data = await response.json();
-        setPublicNotes(data);
-      } catch (error) {
-        console.error("Error fetching public notes:", error);
-      }
-    };
+    if (!fetchedPages.current.has(page)) {
+      fetchPublicNotes(page);
+    }
+  }, [page]);
 
-    fetchPublicNotes();
+  useEffect(() => {
+    fetchUserLikes();
   }, []);
+
+  const fetchPublicNotes = async (page) => {
+    try {
+      const response = await fetch(
+        `${API_URL}/api/notes/public?page=${page}&limit=8`
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      if (data.length < 8) {
+        setHasMore(false);
+      }
+      setPublicNotes((prevNotes) => [...prevNotes, ...data]);
+      fetchedPages.current.add(page); // Mark the page as fetched
+    } catch (error) {
+      console.error("Error fetching public notes:", error);
+    }
+  };
+
+  const fetchUserLikes = async () => {
+    try {
+      const response = await fetch(`${API_URL}/api/notes/public/likes`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      setUserLikes(data.map((like) => like.noteId));
+    } catch (error) {
+      console.error("Error fetching user likes:", error);
+    }
+  };
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const handleLike = async (noteId) => {
+    console.log(`Liked note with ID: ${noteId}`);
+    try {
+      const response = await fetch(
+        `${API_URL}/api/notes/public/${noteId}/like`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+
+      setPublicNotes((prevNotes) =>
+        prevNotes.map((note) =>
+          note._id === noteId ? { ...note, likeCount: data.likeCount } : note
+        )
+      );
+      fetchUserLikes();
+    } catch (error) {
+      console.error("Error handling like:", error);
+    }
+  };
 
   return (
     <div className="public-area">
-      {publicNotes.map((note, index) => (
-        <div key={note._id} className="public-note">
-          <div className="public-note-header">
-            {`ID: ${note._id} - Published on: ${new Date(
-              note.date
-            ).toLocaleDateString()}`}
+      <h2 style={{ marginBottom: "30px" }}>Public Diary Pages</h2>
+      <div className="public-notes-container">
+        {publicNotes.map((note) => (
+          <div key={note._id} className="public-note">
+            <div className="public-note-header">
+              {`ID: ${note._id} - Published on: ${new Date(
+                note.date
+              ).toLocaleDateString()}`}
+            </div>
+            <div className="public-note-content">{note.content}</div>
+            <div className="like-section">
+              <button
+                onClick={() => handleLike(note._id)}
+                className="like-button"
+                disabled={userLikes.includes(note._id)}
+              >
+                {userLikes.includes(note._id) ? "Unlike" : "Like"}
+              </button>
+              <div className="like-count">{note.likeCount} Likes</div>
+            </div>
           </div>
-          <div className="public-note-content">{note.content}</div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      {hasMore ? (
+        <button onClick={handleLoadMore} className="load-more-button">
+          Load More
+        </button>
+      ) : (
+        <div className="no-more-notes">No more pages to load</div>
+      )}
     </div>
   );
 };
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Timeline from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+const CustomTimeline = ({ notes }) => {
+  // Check if notes data is undefined or empty
+  if (!notes || notes.length === 0) {
+    return <div>No notes available</div>;
+  }
+
+  return (
+    <Timeline lineColor={"#ddd"}>
+      {notes.map((group) => (
+        <Timeline.Item key={group.date} dateText={group.date}>
+          {group.notes.map((note) => (
+            <div key={note._id}>
+              <p>{note.content}</p>
+              <Link to={`/private/${note._id}`}>View Note</Link>
+            </div>
+          ))}
+        </Timeline.Item>
+      ))}
+    </Timeline>
+  );
+};
+
+export default CustomTimeline;
